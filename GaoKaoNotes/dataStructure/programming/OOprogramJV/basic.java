3e7178dd093b8229d3d22bf914cca1cdac2778de
j62ul3fu6 <j62ul3fu6@gmail.com>
2025-12-22 16:34:18 +0800

    add some java notes

--- GaoKaoNotes/dataStructure/programming/OOprogramJV/basic.java
+++ GaoKaoNotes/dataStructure/programming/OOprogramJV/basic.java
@@ -5,7 +5,7 @@
 public class basic {  // public class randomName {  ----> this also works but not recommended at all.// 
 	// public使此method在該class或instance以外皆可讀, static使得該method不須建立instance即可以用. C++和C#都差不多
     // static 修飾詞的意思其實就是指該function是屬於class而不是instance(雖然你可以用instance去call這個function, 但編譯器會自動轉成為class所call. 其實c++和c#幾乎一樣(C++多一點點額外的東西)
-    // (String[] args) 裡面的東西其實就是一個陣列宣告. 他表示args這個變數是一個string陣列, 與C#一樣的表示法. 但與C和C++正好相反, 在C和C++中, 會使用(String args[]), 常見的例子就是int testArr[];這樣的宣告. 但是在java和C#中就是int[] testArr;剛好相反, 這只是語法不同而已
+    // (String[] args) 裡面的東西其實就是一個陣列宣告. 他表示args這個變數是一個string陣列, 與C#一樣的表示法. 但與C和C++正好相反, 在C和C++中, 會使用(String args[]), 常見的例子就是int testArr[];這樣的宣告. 但是在java和C#中就是int[] testArr;剛好相反, 這只是語法不同而已.  而在main裡面的參數其實就是你執行.exe的時候要給他參數例如"./test.exe -args", 就算不是手動執行, 而是其他program執行也是一樣, 你要為給他args, 這在C, C++, C#都是一樣的, 只是在練習C的時候通常不會需要參數(正是專案不一定). 而java和C++通常寫法上會預設給main參數, 在C#中你不給其實完全也可以. 但是java中必須要, 因為java設計成執行compiled program時必定會至少吃一個string[], 即使是空的(不是NULL). 
     // 其實是這樣的: c++ came from c, And C# came from java which came from c++. 這是語言演化脈絡
 	public static void main(String[] args) {   
 		System.out.println("hello, world.");
